cc6113cbf94ab18fa0a55f0a03edaaac71dc1fce
Tim Johnston <tedwardj11@gmail.com>
2021-02-05 20:05:54 -0500

    beginning to implement js

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
     // --------------------------------------------------------------------------------------------
     
     //Attach click event listener to any submit button
+    $(".btn-submit-entry").click(function () {
+        submitLogEntry($this);
+    })
 
     //Load any existing log entries
+    loadLogEntries();
 
     // End page initialization and event listeners ------------------------------------------------
 
@@ -21,15 +25,33 @@ $(document).ready(function() {
 //Accepts an optional argument which is the id of a specific log entry. Note that the controller endpoint
 //also accepts this as an optional argument - electing to not pass it in to this method will default it
 //to null here and in the controller and will cause ALL entries to be returned
-//function loadLogEntries() {
+function loadLogEntries(entryId = null) {
 
-//}
+}
 
 //Submits with a POST request a user's log entry. Accepts one argument, the clicked button, which is used
 //to determine whether the entry will be submitted with or without a prepared statement
-//function submitLogEntry(clickedButton) {
+function submitLogEntry(clickedButton) {
 
-//}
+    //Get data from form, determine whether to write to database with or without prepared statement,
+    //and serialize to json
+    var entry = {};
+    entry["user"] = $("#input-user").val();
+    entry["message"] = $("#input-message").val();
+
+    //Determine whether to write to database with or without prepared statement
+    if ($(this).attr("id") === "btn-submit-prepared") {
+        entry["submitType"] = "prepared";
+    } else if ($(this).attr("id") === "btn-submit-prepared") {
+        entry["submitType"] = "injection";
+    } else {
+        console.error("I award you no points, and may God have mercy on your soul.")
+    }
+
+    //Configure and submit ajax request
+    var entryJson = JSON.stringify(entry);
+
+}
 
 //Append log entries to to the guest log <table> so they display on the page
 //Accepts one argument, an array of entries to be displayed
